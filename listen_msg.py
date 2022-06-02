0881122d465e5cf9b03681d16767f4524064d1f5
pqminh27 <minh8a1hb@gmail.com>
2022-06-02 19:37:58 +0300

    First update of Slurm Telegram Bot

---
+++ listen_msg.py
@@ -0,0 +1,77 @@
+import os
+import json
+import time
+from datetime import datetime 
+import requests
+
+TOKEN_API = "5372374960:AAGWFq5WLfldVTg-EvFJG6zao_4qbjDdQtw"
+
+
+# https://api.telegram.org/bot{TOKEN_API}/sendMessage?chat_id=<chat_id>&text=Привет%20мир
+
+# import re
+# datetime_format = "%Y-%m-%dT%H:%M:%S"
+# datetime_format_regex = re.compile(r'^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$')
+# def datetime_parser(dct):
+#     for k, v in dct.items():
+#         if isinstance(v, str) and datetime_format_regex.match(v):
+#             dct[k] = datetime.strptime(v, datetime_format)
+#     return dct
+
+# time_now = datetime.now()
+# current_time_now = time_now.strftime('%Y-%m-%d %I:%M:%S %p')
+# print(current_time_now)
+
+
+def read_file_json(filename):
+    with open(filename, "r") as f:
+        data = json.load(f)
+        return data
+
+# def get_chat_id_from_db():
+chat_id = 1210670436
+
+
+def command_squeue():
+    os.system("squeue --json > squeue.json")
+def get_info_squeue():
+    command_squeue()
+    data = read_file_json("squeue.json")
+    # time_stamp_now = int(round(datetime.now().timestamp()))
+    current_time = datetime.now()
+    time_stamp_now = int(round(current_time.timestamp()))
+    current_time_right_format = current_time.strftime('%Y-%m-%d %I:%M:%S %p')
+    # print(current_time_right_format)
+    for job in data["jobs"]:
+        job_id = job["job_id"]
+        job_name = job["name"]
+        # job_partition = job["partition"]
+        # submit_time = datetime.fromtimestamp(job["submit_time"]).strftime('%Y-%m-%d %I:%M:%S %p')
+        # job_state = job["job_state"]
+        # if job["start_time"] != 0:
+        # start_time = datetime.fromtimestamp(job["start_time"]).strftime('%Y-%m-%d %I:%M:%S %p')
+        # if job["end_time"] != 0:
+        # end_time = datetime.fromtimestamp(job["end_time"]).strftime('%Y-%m-%d %I:%M:%S %p')
+        # print("job_id: " + str(job_id))
+        # print("name of job: " + job_name)
+        # print("partition: "+job_partition)
+        # print("submit time: " + submit_time)
+        # print("state: " + job_state)
+
+        # print(job["start_time"] - job["end_time"])
+        start_time = datetime.fromtimestamp(job["start_time"]).strftime('%Y-%m-%d %I:%M:%S %p')
+        end_time = datetime.fromtimestamp(job["end_time"]).strftime('%Y-%m-%d %I:%M:%S %p')
+        if job["start_time"]-time_stamp_now >= -1 and job["start_time"]-time_stamp_now <= 1:
+            start_msg = f"Your {job_name} job (id: {job_id}) started running at {start_time}!"
+            URL = "https://api.telegram.org/bot"+TOKEN_API+"/sendMessage?chat_id="+str(chat_id)+"&text="+start_msg
+            requests.get(url=URL)
+            # https://api.telegram.org/bot5372374960:AAGWFq5WLfldVTg-EvFJG6zao_4qbjDdQtw/sendMessage?chat_id=1210670436&text=%22Test%22
+            
+        if job["end_time"]-time_stamp_now >= -1 and job["end_time"]-time_stamp_now <= 1:
+            end_msg = f"Your {job_name} job (id: {job_id}) ended at {end_time}!"
+            URL = "https://api.telegram.org/bot"+TOKEN_API+"/sendMessage?chat_id="+str(chat_id)+"&text="+end_msg
+            requests.get(url=URL)
+
+while True:
+    get_info_squeue()
+    time.sleep(1)
---
+++ slurm_bot.py
@@ -0,0 +1,192 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, Message, CallbackQuery
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler, CallbackQueryHandler, MessageHandler, CallbackContext, Filters
+import sys
+import json
+import requests
+import re
+import os
+from datetime import datetime
+import time
+
+TOKEN_API = "5372374960:AAGWFq5WLfldVTg-EvFJG6zao_4qbjDdQtw"
+
+def start_command(update: Update, context: CallbackContext):
+    update.message.reply_text(f"Welcome {update.effective_user.first_name} to Slurm Bot!!\nYou can call /help for more details!\nYour chat_id is {update.effective_user.id}")
+
+def help_command(update: Update, context: CallbackContext):
+    update.message.reply_text(f"All the commands:\n/start : To start;\n/help : To see all the commands;\n/sinfo : To see the information of sinfo;\n/squeue_all : See the squeue of jobs of all users;\n/squeue_your_jobs: See the squeue of your jobs;\n/scontrol {{job_id}} : See the scontrol show job id (id of job);\n/subscribe : To start receiving information about starting and ending jobs;\n/unsubscribe : To stop receiving information about starting and ending jobs")
+
+
+def read_file_json(filename):
+    with open(filename, "r") as f:
+        data = json.load(f)
+        return data
+
+def command_sinfo():
+    os.system(f"sinfo --json > sinfo.json")
+def get_info_sinfo(update: Update, context: CallbackContext):
+    command_sinfo()
+    data = read_file_json("sinfo.json")
+    nodes_count = len(data["nodes"])
+    if nodes_count > 0:
+        for node in data["nodes"]:
+            text =  "Partition: " + node["partitions"][0] + "\nState: "+node["state"] + "\nHostname: " + node["hostname"]+ "\nNodes: " + str(nodes_count)+"\nResources: "+node["tres"]
+            update.message.reply_text(text)
+        # return data
+    else: update.message.reply_text("There is 0 Node running now!")
+
+def command_squeue_json():
+    os.system("squeue --json > squeue.json")
+def get_info_squeue_from_json(update: Update, context: CallbackContext):
+    command_squeue_json()
+    data = read_file_json("squeue.json")
+    if len(data["jobs"]) == 0:
+        update.message.reply_text("There is no job of all users running right now!")
+    else:
+        for job in data["jobs"]:
+            user_name = job["current_working_directory"].split("/")[2]
+            user_id = job["user_id"]
+            job_id = job["job_id"]
+            job_name = job["name"]
+            job_nodes = job["nodes"]
+            job_partition = job["partition"]
+            submit_time = datetime.fromtimestamp(job["submit_time"]).strftime('%Y-%m-%d %I:%M:%S %p')
+            job_state = job["job_state"]
+            # if job["start_time"] != 0:
+            start_time = datetime.fromtimestamp(job["start_time"]).strftime('%Y-%m-%d %I:%M:%S %p')
+            # if job["end_time"] != 0:
+            end_time = datetime.fromtimestamp(job["end_time"]).strftime('%Y-%m-%d %I:%M:%S %p')
+            text = f"Username: {user_name}\nUser_id: {user_id}\nJob_id: {str(job_id)}\nJob name: {job_name}\nNodes: {job_nodes}\nPartition: {job_partition}\nSubmit time: {submit_time}\nState: {job_state}\nStart time: {start_time}\nEnd time: {end_time}"
+            update.message.reply_text(text)
+
+            # update.message.reply_text(f"job_id: {str(job_id)}")
+            # update.message.reply_text(f"name of job: {job_name}")
+            # update.message.reply_text(f"partition: {job_partition}")
+            # update.message.reply_text(f"submit time: {submit_time}")
+            # update.message.reply_text(f"state:  {job_state}")
+            # if(job_state == "CANCELED"):
+            #     update.message.reply_text(f"Your job is canceled!")
+
+            # current_time_now = datetime.now().strftime('%Y-%m-%d %I:%M:%S %p')
+            # print(current_time_now)
+            # start_time = datetime.fromtimestamp(job["start_time"]).strftime('%Y-%m-%d %I:%M:%S %p')
+            # if current_time_now == start_time:
+            #     update.message.reply_text("Your job starts now!")
+            #     update.message.reply_text(f"start time: {start_time}")
+
+            # current_time_now = datetime.now().strftime('%Y-%m-%d %I:%M:%S %p')
+            # end_time = datetime.fromtimestamp(job["end_time"]).strftime('%Y-%m-%d %I:%M:%S %p')
+            # if current_time_now == end_time:
+            #     update.message.reply_text("Your job just ended!")
+            #     update.message.reply_text(f"end time: {end_time}")
+            # print()
+
+def command_squeue(username):
+    command = f"squeue --states=all --user={username} > squeue.txt"
+    os.system(command)
+def get_info_squeue_from_txt(update: Update, context: CallbackContext):
+    
+    #db to get username
+    username = "pm"
+    ####################
+    
+    command_squeue(username)
+    # print(os.path.exists("squeue.txt"))
+    # Check to see if file exist or not
+    # if os.path.exists("squeue.txt") == False:
+    #     update.message.reply_text("You don't have any job running now!")
+    # else:
+    f = open("squeue.txt","r")
+    f.readline()
+    lines = f.readlines()
+    f.close()
+    for line in lines:
+        datas = list(filter(lambda w: (w != ""), line.split(" ")))
+        update.message.reply_text(f"Username: {datas[3]}\nJob_id: {datas[0]}\nJob name: {datas[2]}\nPartition: {datas[1]}\nState: {datas[4]}\nNodes: {datas[6]}\nRunning time: {datas[5]}\nReason: {datas[-1]}")
+    
+    os.system("rm -rf squeue.txt")
+
+
+def command_scontrol_show_job_jobid(job_id):
+    os.system(f"scontrol show job {job_id} > job_{job_id}.txt")
+def get_info_scontrol_show_job_jobid(update: Update, context: CallbackContext):
+    # update.message.reply_text("Type your job id for scontrol show job:")
+    job_id = update.message.text.split()[1]
+    # while update.message.text.isnumeric() == False:
+        # job_id = update.message.text
+        # update.message.reply_text("You just entered not number id!!!")
+    if job_id.isnumeric() == True: 
+        # job_id = update.message.text
+        command_scontrol_show_job_jobid(job_id)
+        if os.stat(f"job_{job_id}.txt").st_size == 0:
+            update.message.reply_text("You just entered invalid job id!!")
+            # os.system(f"rm -rf job_{job_id}.txt")
+        else:
+            f = open(f"job_{job_id}.txt", "r")
+            data = f.read()
+            f.close()
+            update.message.reply_text(data)
+        os.system(f"rm -rf job_{job_id}.txt")
+    else:
+        update.message.reply_text("You must enter your job id after /scontrol !!")
+
+
+def error_handler(update: Update, context: CallbackContext):
+    print(f"ERROR: {context.error} caused by {update}")
+
+# def button(update, context):
+#     query: CallbackQuery = update.callback_query
+#     query.answer()
+#     query.edit_message_text(text="Selected option: {}".format(query.data))
+
+# def send_message_start_or_end_job():
+#     command_squeue()
+#     data = read_file_json("squeue.json")
+#     if len(data["jobs"]) == 0:
+#         print("There is no Job running now!")
+#     else:
+#         current_time_now = datetime.now().strftime('%Y-%m-%d %I:%M:%S %p')
+#         for job in data["jobs"]:
+#             start_time = datetime.fromtimestamp(job["start_time"]).strftime('%Y-%m-%d %I:%M:%S %p')
+#             end_time = datetime.fromtimestamp(job["end_time"]).strftime('%Y-%m-%d %I:%M:%S %p')
+#             if(current_time_now == start_time):
+#                 print(f"Your job started running at {start_time}!")
+#             if(current_time_now == end_time):
+#                 print(f"Your job ended at {end_time}")
+
+#def get_subscribe_status_in_db():
+subscribe_to_msg = False
+
+#Change subscribe at db to True
+def subscribe_receive_job_message(update: Update, context: CallbackContext):
+    subscribe_to_msg = True
+    update.message.reply_text("You will receive notifications about your jobs from now!!")
+
+#Change subscribe at db to False
+def unsubscribe_receive_job_message(update: Update, context: CallbackContext):
+    subscribe_to_msg = False
+    update.message.reply_text("You will stop receiving notifications about your jobs!!")
+
+def main():
+    #persistence=PicklePersistence(filename="bot_data")
+    updater: Updater = Updater(TOKEN_API, use_context=True)
+
+    updater.dispatcher.add_handler(CommandHandler("start", start_command))  # handling /start command
+    updater.dispatcher.add_handler(CommandHandler("help", help_command))  # handling /help command
+    updater.dispatcher.add_handler(CommandHandler("sinfo", get_info_sinfo)) #handling /sinfo command
+    updater.dispatcher.add_handler(CommandHandler("squeue_all", get_info_squeue_from_json)) #handling /squeue commands
+    updater.dispatcher.add_handler(CommandHandler("squeue_your_jobs", get_info_squeue_from_txt)) #handling /squeue --user=username command
+    updater.dispatcher.add_handler(CommandHandler("scontrol", get_info_scontrol_show_job_jobid)) #handling /scontrol command
+    
+    # updater.dispatcher.add_handler(MessageHandler)
+    updater.dispatcher.add_handler(CommandHandler("subscribe", subscribe_receive_job_message)) #handling /subscribe command
+    updater.dispatcher.add_handler(CommandHandler("unsubscribe", unsubscribe_receive_job_message)) #handling /unsubscribe command
+
+    updater.dispatcher.add_error_handler(error_handler)
+    updater.start_polling()
+
+    # Keep the program running until interrupted
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
